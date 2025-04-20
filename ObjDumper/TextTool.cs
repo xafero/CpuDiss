5c619c4d6452c287c145f39b16e475db2d92f779
xafero <xafero@users.noreply.github.com>
2025-04-20 11:36:27 +0200

    Adding fun

--- ObjDumper/TextTool.cs
+++ ObjDumper/TextTool.cs
@@ -8,5 +8,10 @@ namespace ObjDumper
         {
             return string.Join("", bytes.Select(b => $"{b:x2}"));
         }
+
+        public static string TrimOrNull(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text) ? null : text.Trim();
+        }
     }
 }
