6f11bc4ae0de015ec7ac96b0f46df2125fa0a278
xafero <xafero@users.noreply.github.com>
2025-04-25 18:30:14 +0200

    Adding comments

--- ObjDumper/Core/X86.cs
+++ ObjDumper/Core/X86.cs
@@ -177,14 +177,29 @@ namespace ObjDumper.Core
         /// </summary>
         IRET,
 
+        /// <summary>
+        /// Jump if above (unsigned greater than)
+        /// </summary>
         JA,
 
+        /// <summary>
+        /// Jump if above or equal (unsigned greater than or equal)
+        /// </summary>
         JAE,
 
+        /// <summary>
+        /// Jump if below (unsigned less than)
+        /// </summary>
         JB,
 
+        /// <summary>
+        /// Jump if below or equal (unsigned less than or equal)
+        /// </summary>
         JBE,
 
+        /// <summary>
+        /// Jump if carry (CF=1)
+        /// </summary>
         JC,
 
         /// <summary>
@@ -192,14 +207,29 @@ namespace ObjDumper.Core
         /// </summary>
         JCXZ,
 
+        /// <summary>
+        /// Jump if equal (ZF=1)
+        /// </summary>
         JE,
 
+        /// <summary>
+        /// Jump if greater (signed greater than)
+        /// </summary>
         JG,
 
+        /// <summary>
+        /// Jump if greater or equal (signed greater than or equal)
+        /// </summary>
         JGE,
 
+        /// <summary>
+        /// Jump if less (signed less than)
+        /// </summary>
         JL,
 
+        /// <summary>
+        /// Jump if less or equal (signed less than or equal)
+        /// </summary>
         JLE,
 
         /// <summary>
@@ -207,44 +237,104 @@ namespace ObjDumper.Core
         /// </summary>
         JMP,
 
+        /// <summary>
+        /// Jump if not above (unsigned not greater than)
+        /// </summary>
         JNA,
 
+        /// <summary>
+        /// Jump if not above or equal (unsigned not greater than or equal)
+        /// </summary>
         JNAE,
 
+        /// <summary>
+        /// Jump if not below (unsigned not less than)
+        /// </summary>
         JNB,
 
+        /// <summary>
+        /// Jump if not below or equal (unsigned not less than or equal)
+        /// </summary>
         JNBE,
 
+        /// <summary>
+        /// Jump if not carry (CF=0)
+        /// </summary>
         JNC,
 
+        /// <summary>
+        /// Jump if not equal (ZF=0)
+        /// </summary>
         JNE,
 
+        /// <summary>
+        /// Jump if not greater (signed not greater than)
+        /// </summary>
         JNG,
 
+        /// <summary>
+        /// Jump if not greater or equal (signed not greater than or equal)
+        /// </summary>
         JNGE,
 
+        /// <summary>
+        /// Jump if not less (signed not less than)
+        /// </summary>
         JNL,
 
+        /// <summary>
+        /// Jump if not less or equal (signed not less than or equal)
+        /// </summary>
         JNLE,
 
+        /// <summary>
+        /// Jump if not overflow (OF=0)
+        /// </summary>
         JNO,
 
+        /// <summary>
+        /// Jump if not parity (PF=0)
+        /// </summary>
         JNP,
 
+        /// <summary>
+        /// Jump if not sign (SF=0)
+        /// </summary>
         JNS,
 
+        /// <summary>
+        /// Jump if not zero (ZF=0)
+        /// </summary>
         JNZ,
 
+        /// <summary>
+        /// Jump if overflow (OF=1)
+        /// </summary>
         JO,
 
+        /// <summary>
+        /// Jump if parity (PF=1)
+        /// </summary>
         JP,
 
+        /// <summary>
+        /// Jump if parity even (PF=1)
+        /// </summary>
         JPE,
 
+        /// <summary>
+        /// Jump if parity odd (PF=0)
+        /// </summary>
         JPO,
 
+        /// <summary>
+        /// Jump if sign (SF=1)
+        /// </summary>
         JS,
 
+        /// <summary>
+        /// Jump if zero (ZF=1)
+        /// </summary>
         JZ,
 
         /// <summary>
@@ -292,12 +382,24 @@ namespace ObjDumper.Core
         /// </summary>
         LOOP,
 
+        /// <summary>
+        /// Loop while equal (ZF=1)
+        /// </summary>
         LOOPE,
 
+        /// <summary>
+        /// Loop while not equal (ZF=0)
+        /// </summary>
         LOOPNE,
 
+        /// <summary>
+        /// Loop while not zero (ZF=0)
+        /// </summary>
         LOOPNZ,
 
+        /// <summary>
+        /// Loop while zero (ZF=1)
+        /// </summary>
         LOOPZ,
 
         /// <summary>
@@ -395,14 +497,29 @@ namespace ObjDumper.Core
         /// </summary>
         RCR,
 
+        /// <summary>
+        /// Repeat string operation while CX not zero
+        /// </summary>
         REP,
 
+        /// <summary>
+        /// Repeat string operation while equal (ZF=1) and CX not zero
+        /// </summary>
         REPE,
 
+        /// <summary>
+        /// Repeat string operation while not equal (ZF=0) and CX not zero
+        /// </summary>
         REPNE,
 
+        /// <summary>
+        /// Repeat string operation while not zero (ZF=0) and CX not zero
+        /// </summary>
         REPNZ,
 
+        /// <summary>
+        /// Repeat string operation while zero (ZF=1) and CX not zero
+        /// </summary>
         REPZ,
 
         /// <summary>
