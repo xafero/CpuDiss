417304719516b25cbae9da032e2e5b0d5141cd28
xafero <xafero@users.noreply.github.com>
2025-04-26 02:09:40 +0200

    Adding core proc back

--- ObjDumper/HitachiGen.cs
+++ ObjDumper/HitachiGen.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ObjDumper.Core;
+using System.Diagnostics;
+using System.IO;
 
 namespace ObjDumper
 {
@@ -15,5 +17,24 @@ namespace ObjDumper
         public string OutDir { get; }
 
         public IEnumerable<Sh3> Codes => Enum.GetValues<Sh3>().Except([Sh3.None]);
+
+        private static void StartSh3(byte[] bytes, IDictionary<string, ParsedLine> res,
+            string dir, string id)
+        {
+            if (res.ContainsKey(id)) return;
+            var name = Path.Combine(dir, $"sh3_{id}.bin");
+            File.WriteAllBytes(name, bytes);
+            var info = new ProcessStartInfo
+            {
+                FileName = "sh-elf-objdump",
+                ArgumentList = { "-D", "-b", "binary", "-m", "sh3", "-z", name }
+            };
+            if (!File.Exists(name))
+                return;
+            var itm = ObjDumper.ProcTool.StartAndGet(info);
+            if (itm != null)
+                res[id] = itm;
+            File.Delete(name);
+        }
     }
 }
--- ObjDumper/IntelGen.cs
+++ ObjDumper/IntelGen.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ObjDumper.Core;
+using System.Diagnostics;
+using System.IO;
 
 namespace ObjDumper
 {
@@ -15,5 +17,24 @@ namespace ObjDumper
         public string OutDir { get; }
 
         public IEnumerable<X86> Codes => Enum.GetValues<X86>().Except([X86.None]);
+
+        private static void StartI86(byte[] bytes, IDictionary<string, ParsedLine> res,
+            string dir, string id)
+        {
+            if (res.ContainsKey(id)) return;
+            var name = Path.Combine(dir, $"i86_{id}.bin");
+            File.WriteAllBytes(name, bytes);
+            var info = new ProcessStartInfo
+            {
+                FileName = "objdump",
+                ArgumentList = { "-D", "-Mintel,i8086", "-b", "binary", "-m", "i386", "-z", name },
+            };
+            if (!File.Exists(name))
+                return;
+            var itm = ObjDumper.ProcTool.StartAndGet(info);
+            if (itm != null)
+                res[id] = itm;
+            File.Delete(name);
+        }
     }
 }
